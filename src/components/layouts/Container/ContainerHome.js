96fc753e3bcd857a890b72bdf303c4b4deff6877
Kinghood15 <annguyen8278163@gmail.com>
2022-07-25 09:45:19 +0700

    Update file header.css and containerhome.js

--- src/components/layouts/Container/ContainerHome.js
+++ src/components/layouts/Container/ContainerHome.js
@@ -11,18 +11,20 @@ const ContainerHome = () => {
                     <img src={banner} alt="banner" className="w-full h-auto rounded" />
                 </div>
                 <div className="text-box absolute top-[60%] bigMobile:top-[50%] mobile:top-[30%] smallMobile:top-[20%] w-[95%] ">
-                    <h1 className="text-9xl tablet:text-8xl bigMobile:text-6xl mobile:text-4xl smallMobile:text-2xl leading-[140px] tablet:leading-[140px] bigMobile:leading-[100px] mobile:leading-[90px] smallMobile:leading-[70px]">Buy & exchange </h1>
+                    <h1 className="text-9xl tablet:text-8xl bigMobile:text-6xl mobile:text-4xl smallMobile:text-2xl leading-[120px] tablet:leading-[120px] bigMobile:leading-[100px] mobile:leading-[90px] smallMobile:leading-[70px]">Buy & exchange </h1>
                     <h2>crypto tokens instantly.</h2>
                     <h3 className="w-[95%] laptop:w-[90%] text-justify text-base smallMobile:text-sm smallMobile:text-sm mt-5 smallMobile:leading ">Onechain is the only crypto wallet than can seamlessly move Ethereum between your bank account and decentralized exchanges like uniswap.</h3>
                 </div>
             </div>
-            <div className="containerRight w-full laptop:w-1/2 h-full mobile:h-auto smallMobile:h-auto mobile:py-5 smallMobile:py-5 flex items-center justify-center">
+            <div className="containerRight w-full laptop:w-1/2 h-full mobile:h-auto smallMobile:h-auto mobile:py-8 smallMobile:py-8 flex items-center justify-center">
                 <div className="boxBuy w-[95%] bigMobile:w-[85%] tablet:w-[80%] h-auto p-3">
                     <div className="boxBuyHeader border-b p-3 ">
-                        <div className="rounded-xl bg-sky-600 w-32 h-24 flex items-center justify-center"><img src={logo280px} alt="logo" className="w-28 h-16" /></div>
+                        <div className="rounded-xl w-32 h-24 flex items-center justify-center">
+                            <img src={logo280px} alt="logo" className="w-28 h-16" />
+                        </div>
                     </div>
                     <div className="boxBuyBody py-5">
-                        <div className="boxTime my-3 w-full p-3 rounded-xl bg-white h-auto block mobile:flex mobile:justify-between">
+                        <div className="boxTime my-3 w-full p-3 rounded-xl bg-white h-auto block bigMobile:flex bigMobile:justify-between">
                             <div className="p-2 bg-white bigMobile:bg-gray-100 rounded-2xl">
                                 <div className="flex items-center">
                                     <h3 className="font-bold text-xs tablet:text-lg">Start time:</h3>
@@ -38,7 +40,7 @@ const ContainerHome = () => {
                                 <p className="text-sm tablet:text-base text-gray-600">8/23/2024, 8:24:09 PM</p>
                             </div>
                         </div>
-                        <div className="boxDisplayBody my-3 w-full p-3 rounded-xl bg-white h-auto block  mobile:justify-between mobile:flex">
+                        <div className="boxDisplayBody my-3 w-full p-3 rounded-xl bg-white h-auto block  bigMobile:justify-between bigMobile:flex">
                             <div className="p-2">
                                 <h3 className="font-bold text-xs tablet:text-lg w-auto h-auto bigMobile:h-auto mobile:h-10 smallMobile:h-8">Total Supply:</h3>
                                 <p className="text-gray-600 text-base">10000000.0</p>
